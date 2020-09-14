1f2cfdf2f676cd4dd0c1220ef242b4509d96f1c5
Morozov-5F <morozov.evgeniy.95@gmail.com>
2020-09-14 22:05:07 +0300

    [3.1-3.3] Don't pass vectors by reference to the INLINE functions. - Come on, that was stupid.

--- vec3.h
+++ vec3.h
@@ -39,42 +39,42 @@ static inline void vec3_scale(vec3_t *a, double scalar)
     a->z *= scalar;
 }
 
-static inline double vec3_length_squared(const vec3_t *v)
-{ return v->x * v->x + v->y + v->y + v->z * v->z; }
+static inline double vec3_length_squared(vec3_t v)
+{ return v.x * v.x + v.y + v.y + v.z * v.z; }
 
-static inline double vec3_length(const vec3_t *v)
+static inline double vec3_length(vec3_t v)
 { return sqrt(vec3_length_squared(v)); }
 
-static inline vec3_t vec3_sum(const vec3_t *a, const vec3_t *b)
-{ return vec3(a->x + b->x, a->y + b->y, a->z + b->z); }
+static inline vec3_t vec3_sum(vec3_t a, vec3_t b)
+{ return vec3(a.x + b.x, a.y + b.y, a.z + b.z); }
 
-static inline vec3_t vec3_diff(const vec3_t *a, const vec3_t *b)
-{ return vec3(a->x - b->x, a->y - b->y, a->z - b->z); }
+static inline vec3_t vec3_diff(vec3_t a, vec3_t b)
+{ return vec3(a.x - b.x, a.y - b.y, a.z - b.z); }
 
-static inline vec3_t vec3_multiply(const vec3_t *a, const vec3_t *b)
-{ return vec3(a->x * b->x, a->y * b->y, a->z * b->z); }
+static inline vec3_t vec3_multiply(vec3_t a, vec3_t b)
+{ return vec3(a.x * b.x, a.y * b.y, a.z * b.z); }
 
-static inline vec3_t vec3_multiply_scalar(const vec3_t *a, double s)
-{ return vec3(a->x * s, a->y * s, a->z * s); }
+static inline vec3_t vec3_multiply_scalar(vec3_t a, double s)
+{ return vec3(a.x * s, a.y * s, a.z * s); }
 
-static inline double vec3_dor(const vec3_t *a, const vec3_t *b)
-{ return a->x * b->x + a->y * b->y + a->z * b->z; }
+static inline double vec3_dor(vec3_t a, vec3_t b)
+{ return a.x * b.x + a.y * b.y + a.z * b.z; }
 
-static inline vec3_t vec3_cross(const vec3_t *a, const vec3_t *b)
+static inline vec3_t vec3_cross(vec3_t a, vec3_t b)
 {
-    return vec3(a->y * b->z - a->z * b->y,
-                a->z * b->x - a->x * b->z,
-                a->x * b->y - a->y * b->x);
+    return vec3(a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x);
 }
 
-static inline vec3_t vec3_normalized(const vec3_t *a)
+static inline vec3_t vec3_normalized(vec3_t a)
 {
     return vec3_multiply_scalar(a, 1.0 / vec3_length(a));
 }
 
-static inline const char * vec3_to_string(const vec3_t *a, char *buffer, size_t max_buffer_len)
+static inline const char * vec3_to_string(vec3_t a, char *buffer, size_t max_buffer_len)
 {
-    if (-1 == snprintf(buffer, max_buffer_len, "%f %f %f", a->x, a->y, a->z))
+    if (-1 == snprintf(buffer, max_buffer_len, "%f %f %f", a.x, a.y, a.z))
     {
         return NULL;
     }
