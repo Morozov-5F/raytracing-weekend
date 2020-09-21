4de4ff5d62b6990d4969a4408038e1284880c682
Morozov-5F <morozov.evgeniy.95@gmail.com>
2020-09-21 22:08:13 +0300

    Update license copyrights.

--- rt_material_diffuse_light.c
+++ rt_material_diffuse_light.c
@@ -1,6 +1,9 @@
-//
-// Created by morozov on 21.09.2020.
-//
+/**
+ * Copyright (c) 2020, Evgeniy Morozov
+ * All rights reserved.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #include <assert.h>
 #include "rt_material_diffuse_light.h"
