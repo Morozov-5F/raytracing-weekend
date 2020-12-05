1fae527b7d87bf1481d51898b8479c6bc7f30879
Morozov-5F <morozov.evgeniy.95@gmail.com>
2020-12-05 12:16:48 +0300

    Add file templates for Idea project.

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,5 @@
+#parse("BSD-Clause.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+#[[#endif]]# //${INCLUDE_GUARD}
---
+++ .idea/fileTemplates/internal/C Source File.c
@@ -0,0 +1,4 @@
+#parse("BSD-Clause.h")
+#if (${HEADER_FILENAME})
+#[[#include]]# "${HEADER_FILENAME}"
+#end
