e877684115e9fef1188f8607178eec30bc1480a9
Morozov-5F <morozov.evgeniy.95@gmail.com>
2020-12-05 18:53:01 +0300

    [TheNextWeek][3.10] Fix year in copyright header.

--- rt_aabb.c
+++ rt_aabb.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) ${$YEAR}, Evgeniy Morozov
+ * Copyright (c) 2020, Evgeniy Morozov
  * All rights reserved.
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
