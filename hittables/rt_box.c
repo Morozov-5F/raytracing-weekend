7f340cef403d4603e2fe7bafc245e4c9989ef111
Morozov-5F <morozov.evgeniy.95@gmail.com>
2020-12-15 11:58:57 +0300

    [TheNextWeek] Get rid of BVH node inside the box hittable.

--- hittables/rt_box.c
+++ hittables/rt_box.c
@@ -28,28 +28,26 @@ rt_box_t *rt_box_new(point3_t min, point3_t max, rt_material_t *material)
     result->min = min;
     result->max = max;
 
-    rt_hittable_list_t *sides = rt_hittable_list_init(6);
-
-    // Front and back
-    rt_hittable_list_add(sides, (rt_hittable_t *)rt_aa_rect_new_xy(min.x, max.x, min.y, max.y, min.z, material));
-    rt_hittable_list_add(
-        sides, (rt_hittable_t *)rt_aa_rect_new_xy(min.x, max.x, min.y, max.y, max.z, rt_material_claim(material)));
-    // Top and bottom
-    rt_hittable_list_add(
-        sides, (rt_hittable_t *)rt_aa_rect_new_xz(min.x, max.x, min.z, max.z, min.y, rt_material_claim(material)));
-    rt_hittable_list_add(
-        sides, (rt_hittable_t *)rt_aa_rect_new_xz(min.x, max.x, min.z, max.z, max.y, rt_material_claim(material)));
-    // Left and right
-    rt_hittable_list_add(
-        sides, (rt_hittable_t *)rt_aa_rect_new_yz(min.y, max.y, min.z, max.z, min.x, rt_material_claim(material)));
-    rt_hittable_list_add(
-        sides, (rt_hittable_t *)rt_aa_rect_new_yz(min.y, max.y, min.z, max.z, max.x, rt_material_claim(material)));
-
-    result->sides = rt_hittable_list_init(1);
+    result->sides = rt_hittable_list_init(6);
     assert(NULL != result->sides);
 
-    rt_hittable_list_add(result->sides, (rt_hittable_t *)rt_bvh_node_new(sides, 0, 1));
-    rt_hittable_list_deinit(sides);
+    // Front and back
+    rt_hittable_list_add(result->sides, (rt_hittable_t *)rt_aa_rect_new_xy(min.x, max.x, min.y, max.y, min.z, material));
+    // Left
+    rt_hittable_list_add(
+        result->sides, (rt_hittable_t *)rt_aa_rect_new_yz(min.y, max.y, min.z, max.z, min.x, rt_material_claim(material)));
+    // Right
+    rt_hittable_list_add(
+        result->sides, (rt_hittable_t *)rt_aa_rect_new_yz(min.y, max.y, min.z, max.z, max.x, rt_material_claim(material)));
+    // Top
+    rt_hittable_list_add(
+        result->sides, (rt_hittable_t *)rt_aa_rect_new_xz(min.x, max.x, min.z, max.z, max.y, rt_material_claim(material)));
+    // Bottom
+    rt_hittable_list_add(
+        result->sides, (rt_hittable_t *)rt_aa_rect_new_xz(min.x, max.x, min.z, max.z, min.y, rt_material_claim(material)));
+    // Back
+    rt_hittable_list_add(
+        result->sides, (rt_hittable_t *)rt_aa_rect_new_xy(min.x, max.x, min.y, max.y, max.z, rt_material_claim(material)));
 
     rt_hittable_init(&result->base, RT_HITTABLE_TYPE_BOX);
 
