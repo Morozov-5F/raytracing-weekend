88ec6d80cee9344e24f82a19dabe1a14e4b93b40
Morozov-5F <morozov.evgeniy.95@gmail.com>
2026-05-31 16:07:21 +0300

    Improve rendering on Windows - Use device independent bitmap for displaying the image. - Fix type mismatch on window creation. - Header cleanup.

--- window/rt_window.h
+++ window/rt_window.h
@@ -7,10 +7,8 @@
 
 #pragma once
 
-#include <stddef.h>
 #include <stdbool.h>
-#include <rt_weekend.h>
-#include <rt_vec3.h>
+#include <rt_colour.h>
 
 typedef struct rt_window_s rt_window_t;
 
@@ -22,4 +20,4 @@ void rt_window_process(rt_window_t *window);
 
 void rt_window_close(rt_window_t *window);
 
-bool rt_window_display_image(rt_window_t *window, vec3_t *image, size_t width, size_t height, size_t samples_per_pixel);
+bool rt_window_display_image(rt_window_t *window, colour_t *image, size_t width, size_t height, size_t samples_per_pixel);
--- window/rt_window_win32.c
+++ window/rt_window_win32.c
@@ -17,13 +17,15 @@ struct rt_window_s
 {
     WNDCLASS cls;
     HWND window;
+
+    size_t width, height;
 };
 
 static LRESULT rt_win32_wnd_proc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 rt_window_t *rt_window_init(const char *window_title, size_t width, size_t height)
 {
-    rt_window_t *new_window = (rt_window_t *)calloc(1, sizeof(rt_window_t));
+    rt_window_t *new_window = calloc(1, sizeof(rt_window_t));
     if (NULL == new_window)
     {
         return NULL;
@@ -33,12 +35,14 @@ rt_window_t *rt_window_init(const char *window_title, size_t width, size_t heigh
     new_window->cls.lpfnWndProc = (WNDPROC)rt_win32_wnd_proc;
     new_window->cls.hInstance = GetModuleHandle(0);
     new_window->cls.lpszClassName = "rt_wnd";
+    new_window->width = width;
+    new_window->height = height;
 
     // Then we should register it and get an atom which we can use to create a window later
     ATOM register_result = RegisterClassA(&new_window->cls);
 
     // Now we can create a windows with specified parameters
-    new_window->window = CreateWindow(register_result, window_title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+    new_window->window = CreateWindow(MAKEINTATOM(register_result), window_title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
                                       width, height, NULL, NULL, new_window->cls.hInstance, NULL);
 
     if (NULL == new_window->window)
@@ -72,25 +76,76 @@ void rt_window_close(rt_window_t *window)
 
 }
 
-bool rt_window_display_image(rt_window_t *window, vec3_t *image, size_t width, size_t height, size_t samples_per_pixel)
+static RGBTRIPLE* convert_colour_to_bitmap(const colour_t *image, size_t width, size_t height, size_t samples_per_pixel, BITMAPINFO *bitmap_info)
+{
+    RGBTRIPLE *bitmap = calloc(width * height, sizeof(RGBTRIPLE));
+
+    if (NULL == bitmap || NULL == bitmap_info)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < width * height; i++)
+    {
+        int r, g, b;
+        rt_convert_to_rgb(image[i], samples_per_pixel, &r, &g, &b);
+        bitmap[i].rgbtRed = r;
+        bitmap[i].rgbtGreen = g;
+        bitmap[i].rgbtBlue = b;
+    }
+
+    bitmap_info->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bitmap_info->bmiHeader.biWidth = (LONG)width;
+    bitmap_info->bmiHeader.biHeight = (LONG)height;
+    bitmap_info->bmiHeader.biPlanes = 1;
+    bitmap_info->bmiHeader.biBitCount = 24;
+    bitmap_info->bmiHeader.biCompression = BI_RGB;
+    bitmap_info->bmiHeader.biSizeImage = 0;
+    bitmap_info->bmiHeader.biXPelsPerMeter = 0;
+    bitmap_info->bmiHeader.biYPelsPerMeter = 0;
+    bitmap_info->bmiHeader.biClrUsed = 0;
+    bitmap_info->bmiHeader.biClrImportant = 0;
+
+    return bitmap;
+}
+
+bool rt_window_display_image(rt_window_t *window, colour_t *image, size_t width, size_t height, size_t samples_per_pixel)
 {
     if (NULL == window || NULL == image)
     {
         return false;
     }
 
-    // TODO: This routine is VERY slow, need to optimize it, maybe by displaying the full bitmap?
     HDC dc = GetDC(window->window);
-    for (int j = height - 1; j >= 0; --j)
+    int rasterCaps = GetDeviceCaps(dc, RASTERCAPS);
+
+    BITMAPINFO bitmap_info;
+    RGBTRIPLE *pixel_data = convert_colour_to_bitmap(image, width, height, samples_per_pixel, &bitmap_info);
+    if (NULL == pixel_data)
     {
-        for (int i = 0; i < width; ++i)
-        {
-            int r, g, b;
-            rt_convert_to_rgb(image[j * width + i], samples_per_pixel, &r, &g, &b);
-            SetPixel(dc, i, height - j, RGB(r, g, b));
-        }
+        fprintf(stderr, "Could not convert to RGBTRIPLE\n");
     }
+
+    int res = 0;
+    if (rasterCaps & RC_STRETCHDIB)
+    {
+        res = StretchDIBits(dc, 0, 0, window->width, window->height, 0, 0, width, height, pixel_data, &bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+    }
+    else
+    {
+        res = SetDIBitsToDevice(dc, 0, 0, width, height, 0, 0, 0, height, pixel_data, &bitmap_info, DIB_RGB_COLORS);
+    }
+
+    if (!res)
+    {
+        fprintf(stderr, "SetDIBitsToDevice failed\n");
+    }
+
+    free(pixel_data);
+
     ReleaseDC(window->window, dc);
+
+    return true;
 }
 
 static LRESULT rt_win32_wnd_proc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -108,7 +163,7 @@ static LRESULT rt_win32_wnd_proc(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
             break;
 
         case WM_CLOSE:
-            if (MessageBox(hWnd, "Really quit?", "My application", MB_OKCANCEL) == IDOK)
+            if (MessageBox(hWnd, "Really quit?", "RT Weekend", MB_OKCANCEL) == IDOK)
             {
                 DestroyWindow(hWnd);
             }
